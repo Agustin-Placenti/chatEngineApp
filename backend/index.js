0773f7ff7dfcddf63ad68619bfbf0e634cc9258c
Agustin-Placenti <agustinplacenti@hotmail.com>
2023-04-11 21:57:31 -0300

    chat engine io app

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const { default: axios } = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+
+  try {
+    const response = await axios.put(
+      "https://api.chatengine.io/users/",
+      {
+        username: username,
+        secret: username,
+        first_name: username,
+      },
+      { headers: { "private-key": "  3a1b5577-21c6-41a9-a2bd-7bf992ae5295" } }
+    );
+    return res.status(response.status).json(response.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+  return res.json({ username: username, secret: "sha256..." });
+});
+
+app.listen(3001);
---
+++ frontend/src/ChatsPage.jsx
@@ -0,0 +1,21 @@
+import {
+  MultiChatSocket,
+  MultiChatWindow,
+  useMultiChatLogic,
+} from "react-chat-engine-advanced";
+
+const ChatsPage = (props) => {
+  const chatProps = useMultiChatLogic(
+    "c7e68ecb-7112-45d5-bdd0-d737c1c0750a",
+    props.user.username,
+    props.user.secret
+  );
+  return (
+    <div style={{ height: "100vh" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow {...chatProps} style={{ height: "100%" }} />
+    </div>
+  );
+};
+
+export default ChatsPage;
